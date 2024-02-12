1da0b57ace1552fffcae610ef859d6d5a5703945
Stavros Thalassinos <stavros8003@gmail.com>
2024-02-12 15:08:10 +0200

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.starting_page , name="starting_page"),
+    path('/posts', views.posts, name="posts"),
+    path('post/<slug:slug>/', views.post_detail, name="post_detail"),
+]
