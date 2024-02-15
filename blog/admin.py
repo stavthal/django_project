a95be1052e2736fb66f28bacff7b6b2b51302e95
Stavros Thalassinos <stavros8003@gmail.com>
2024-02-15 16:15:07 +0200

    added author and tag classes

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Post
+
 # Register your models here.
+
+# add me a filter for the PostAdmin
+class PostAdmin(admin.ModelAdmin):
+    # add me a filter for the PostAdmin
+    list_display = ("title", "slug", "date")
+    # add me more sections for the PostAdmin
+    list_filter = ("date",)
+    # add me a search bar for the PostAdmin
+    search_fields = ("title", "content")
+    # add me prefilled fields for the Post
+    prepopulated_fields = {"slug": ("title",)}
+    
+    
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateField()
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+    
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.caption
+    
